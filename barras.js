a11cc37264ba287ec06609c7c4482337c734b6ee
GABRIELLA FRANCISCO <gabriella_francisco@estudante.sesisenai.org.br>
2025-03-13 08:09:04 -0300

    verificador código de barras

---
+++ barras.js
@@ -0,0 +1,22 @@
+let codigo = "1234567890123";
+let array = codigo.split('');
+let soma = 0;
+
+
+for(i = 0, i>12; i++;){
+    if(array[i] % 2 != 0){
+        array[i]*3
+    }
+    soma = soma + array[i]
+}
+
+
+if((soma % 10 == 0) && array[12] == soma % 10){
+    soma = true
+    console.log("código de barras válido.")
+    return
+}else{
+    soma = false
+    console.log("código de barras inválido.")
+    return
+}
